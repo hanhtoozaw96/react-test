038e7f3d3e3ba7c0a7955bea932dad5ffca0fd28
Bryan Zaw <bryanhhz@gmail.com>
2024-08-18 15:11:10 +0700

    first commit

---
+++ src/data/TransactionData.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        transaction_id: 1,
+        transaction_date: "2023-10-01",
+        employee_id: "12345",
+        employee_name: "Sithu Phone Myint",
+        ewa_amount: 3500,
+        ewa_currency: "THB"
+        
+    },
+    {
+        transaction_id: 2,
+        transaction_date: "2023-10-02",
+        employee_id: "67890",
+        employee_name: "Lucas Yang",
+        ewa_amount: 400,
+        ewa_currency: "THB"
+        
+    },
+    {
+        transaction_id: 3,
+        transaction_date: "2023-10-03",
+        employee_id: "11223",
+        employee_name: "Zwe Lynn Ngwe",
+        ewa_amount: 2300,
+        ewa_currency: "THB"
+        
+    },
+    {
+        transaction_id: 4,
+        transaction_date: "2023-10-04",
+        employee_id: "44556",
+        employee_name: "Hein Myat Win Lwin",
+        ewa_amount: 4000,
+        ewa_currency: "THB"
+        
+    },
+    {
+        transaction_id: 5,
+        transaction_date: "2023-10-05",
+        employee_id: "77889",
+        employee_name: "Bryan Zaw",
+        ewa_amount: 2700,
+        ewa_currency: "THB"
+        
+    },
+    {
+        transaction_id: 6,
+        transaction_date: "2023-10-06",
+        employee_id: "99001",
+        employee_name: "Jackson Lim",
+        ewa_amount: 5000,
+        ewa_currency: "THB"
+        
+    }
+]
